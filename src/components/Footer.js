e1d19bb76ff408c05abdeea52f8be1db9c3a8167
mohamed osman mohamud <bahaley@mohameds-MacBook-Air.local>
2023-12-02 21:45:49 -0600

    creating components and setup basic structure of the web

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,25 @@ import React from 'react'
 import './style.css';
 export default function Footer() {
   return (
-    <div className='Footer'>Footer</div>
+    <div className='Footer'>
+        <div className='Footer-pic'>
+            
+        <p>pic</p> <p>pic</p> <p>pic</p> <p>pic</p> <p>pic</p> <p>pic</p> <p>pic</p>
+        </div>
+        <div className='Footer-links'>
+         <p>links</p>
+         <p>links</p>
+         <p>links</p>
+         <p>links</p>
+         <p>links</p>
+         <p>links</p>
+        </div>
+        <div className='Footer-address'>
+            <p>address</p> <p>address</p> <p>address</p> <p>address</p> <p>address</p> <p>address</p> <p>address</p> <p>address</p>
+        </div>
+
+
+
+    </div>
   )
 }
